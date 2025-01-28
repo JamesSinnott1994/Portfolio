1ed2f9c4a352b2c10a75f213c91adeb153a50eff
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2025-01-28 10:22:57 +0000

    [REFACTOR] Dynamically updating year for copyright in footer

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
 // On Load
 var slideIndex = -1;
 
-// Initial call
-homepageSlideShow()
+var pathName = window.location.pathname;
+console.log(pathName);
 
-function homepageSlideShow() {
+if (pathName.endsWith("index.html")) {
+	// Initial call
+	homepageSlideShow()
 
-	// Select the image element
-	var homePageProjectsImage = document.querySelector(".home-section .placeholder");
+	console.log("RUNNING CODE");
 
-	// Will contain a path to the images
-	var homepageSlideshowImages = [
-		"img/spacedodge1.PNG",
-		"img/Fitbit2.png",
-		"img/clashofgnomes.png",
-		"img/rainbowrun1.png",
-	];
+	function homepageSlideShow() {
 
-	// Increment slide index
-	slideIndex++;
-	if(slideIndex === homepageSlideshowImages.length) {
-		slideIndex = 0;
+		// Select the image element
+		var homePageProjectsImage = document.querySelector(".home-section .placeholder");
+
+		// Will contain a path to the images
+		var homepageSlideshowImages = [
+			"img/spacedodge1.PNG",
+			"img/Fitbit2.png",
+			"img/clashofgnomes.png",
+			"img/rainbowrun1.png",
+		];
+
+		// Increment slide index
+		slideIndex++;
+		if(slideIndex === homepageSlideshowImages.length) {
+			slideIndex = 0;
+		}
+
+		// Access image, and change it's image path
+		homePageProjectsImage.src = homepageSlideshowImages[slideIndex];
+
+		// Change image every 10 seconds
+		setTimeout(homepageSlideShow, 10000);
 	}
-
-	// Access image, and change it's image path
-	homePageProjectsImage.src = homepageSlideshowImages[slideIndex];
-
-	// Change image every 10 seconds
-	setTimeout(homepageSlideShow, 10000);
 }
+
+console.log("YEAR");
+document.getElementById('year').textContent = new Date().getFullYear();
